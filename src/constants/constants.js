40f7812d97e69e02f01a81b08db49b70f1fcb415
ngoctrangnguyen1701 <89184893+ngoctrangnguyen1701@users.noreply.github.com>
2022-01-28 20:32:46 +0700

    change variable enviroment

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,5 @@
 //when push code and deploy, change 'LOCAL' to 'HOSTING'
 // export const linkServer = process.env.REACT_APP_LINK_SERVER_LOCAL
-export const linkServer = process.env.REACT_APP_LINK_SERVER_HOSTING
+// export const linkServer = process.env.REACT_APP_LINK_SERVER_HOSTING
+
+export const linkServer = 'https://server-play-game-caro.herokuapp.com'
