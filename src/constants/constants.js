644207797f70176669e7925b87d2db802f463412
ngoctrangnguyen1701 <89184893+ngoctrangnguyen1701@users.noreply.github.com>
2022-01-29 12:13:57 +0700

    npm i env-cmd

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,4 @@
 //when push code and deploy, change 'LOCAL' to 'HOSTING'
 // export const linkServer = process.env.REACT_APP_LINK_SERVER_LOCAL
-export const linkServer = process.env.REACT_APP_LINK_SERVER_HOSTING
+// export const linkServer = process.env.REACT_APP_LINK_SERVER_HOSTING
+export const linkServer = process.env.REACT_APP_BASE_URL
