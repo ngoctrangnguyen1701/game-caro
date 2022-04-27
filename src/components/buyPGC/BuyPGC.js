6cce27af4acc99e4551ef9ae56196f35f0fd8f07
trang <ngoctrangnguyen1701@gmail.com>
2022-04-28 00:17:04 +0700

    buy PGC

--- src/components/buyPGC/BuyPGC.js
+++ src/components/buyPGC/BuyPGC.js
@@ -20,11 +20,12 @@ const BuyPGC = () => {
   const [amountBNB, setAmountBNB] = React.useState(0)
   const [account, setAccount] = React.useState('')
   const [balance, setBalance] = React.useState(0)
+  const [token, setToken] = React.useState(0)
   const [isConnentMetamask, setIsConnentMetamask] = React.useState(false)
   const [web3, setWeb3] = React.useState(null)
   const [pgc, setPGC] = React.useState({
     contract: {},
-    address: '0x1Bb2B5133D4Cd15afc3e459f5E5Df8e299B70dC0',
+    address: '0xDb155F03982Ed455Aa88892E573357509F13394f',
     abi: [
       {
         inputs: [],
@@ -151,6 +152,21 @@ const BuyPGC = () => {
         ],
         payable: false,
         stateMutability: "nonpayable",
+        type: "function"
+      },
+      {
+        constant: true,
+        inputs: [],
+        name: "balanceBNB",
+        outputs: [
+          {
+            internalType: "uint256",
+            name: "",
+            type: "uint256"
+          }
+        ],
+        payable: false,
+        stateMutability: "view",
         type: "function"
       },
       {
@@ -309,6 +325,27 @@ const BuyPGC = () => {
       },
       {
         constant: false,
+        inputs: [
+          {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256"
+          }
+        ],
+        name: "receiveToken",
+        outputs: [
+          {
+            internalType: "bool",
+            name: "",
+            type: "bool"
+          }
+        ],
+        payable: true,
+        stateMutability: "payable",
+        type: "function"
+      },
+      {
+        constant: false,
         inputs: [],
         name: "renounceOwnership",
         outputs: [],
@@ -417,8 +454,32 @@ const BuyPGC = () => {
         payable: false,
         stateMutability: "nonpayable",
         type: "function"
+      },
+      {
+        constant: false,
+        inputs: [],
+        name: "withdrawAllMoney",
+        outputs: [],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function"
+      },
+      {
+        constant: false,
+        inputs: [
+          {
+            internalType: "uint256",
+            name: "amount",
+            type: "uint256"
+          }
+        ],
+        name: "withdrawAmount",
+        outputs: [],
+        payable: false,
+        stateMutability: "nonpayable",
+        type: "function"
       }
-    ],
+    ]
   })
 
   const onConfirmValue = value => {
@@ -456,6 +517,14 @@ const BuyPGC = () => {
         ...pgc,
         contract
       })
+      //gọi methods balanceOf để biết được tài khoản đang có bao nhiêu token
+      //***Lưu ý:
+      //Những hàm tương tác với Smart Contract mà làm thay đổi Blockchain (vd: thực hiện 1 giao dịch) sẽ tốn 1 lượng phí gas
+      // Còn những hàm dùng để tra cứu không làm thay đổi Blockchain(vd: xem số dư) sẽ không bị tốn phí gas
+
+      const balanceWeiOfToken = await contract.methods.balanceOf(account).call()
+      const balanceToken = await connectWeb3.utils.fromWei(balanceWeiOfToken, 'ether')
+      setToken(balanceToken)
     }
   }
 
@@ -474,14 +543,17 @@ const BuyPGC = () => {
     }
 
     try {
+      const amountPGCToWei = await web3.utils.toWei(amountPGC.toString())
+      const amountBNBToWei = await web3.utils.toWei(amountBNB.toString())
+
       const gasPrice = await web3.eth.getGasPrice()
-      const gas = await pgc.contract.methods.transfer(account, amountPGC).estimateGas({
-        gas: 50000,
+      const gas = await pgc.contract.methods.receiveToken(amountPGCToWei).estimateGas({
+        gas: 500000,
         from: account,
-        // value: web3.utils.toWei(amountBNB.toString(), 'ether')
-        value: '0'
+        value: amountBNBToWei //-->biến BNB thành đơn vị Wei
       })
-      const data = await pgc.contract.methods.transfer(account, amountPGC).encodeABI()
+      const data = await pgc.contract.methods.receiveToken(amountPGCToWei).encodeABI()
+      //encondeABI tương đương với việc chuyển đổi thành chuỗi Hex
       const txHash = await window.ethereum.request({
         method: 'eth_sendTransaction',
         params: [{
@@ -489,13 +561,17 @@ const BuyPGC = () => {
           gas: web3.utils.toHex(gas),
           to: pgc.address,
           from: account,
-          // value: web3.utils.toHex(web3.utils.toWei(amountBNB.toString(), 'ether')),
-          value: '0',
+          value: web3.utils.toHex(amountBNBToWei),
           data
         }]
       })
       console.log(txHash);
       toast.success(`Buy ${amountPGC} success`)
+
+      // setup lại các giá trị
+      setAmountPGC(0)
+      setAmountBNB(0)
+      connectMetamask() //--> chạy lại hàm để cập nhật lại balance BNB
     } catch (error) {
       console.log(error);
       toast.error('Buy PGC failed')
@@ -504,11 +580,21 @@ const BuyPGC = () => {
 
   return (
     <div className='mt-4 container'>
-      <Button
-        color='primary'
-        variant='contained'
-        onClick={connectMetamask}
-      >{isConnentMetamask ? `Your wallet: ${balance} BNB` : 'Connect Metamask'}</Button>
+      {isConnentMetamask ? (
+        <div>
+          <h5 className='mb-0'>Address: <span className='text-danger'>{account}</span></h5>
+          <h5 className='mb-0'>Balance: <span className='text-success'>{balance} BNB</span></h5>
+          <h5 className='mb-0'>Token: <span className='text-secondary'>{token} PGC</span></h5>
+        </div>
+        ) : (
+          <Button
+            color='primary'
+            variant='contained'
+            onClick={connectMetamask}
+          >Connect Metamask</Button>
+        )
+      }
+      
       <div className='bg-dark col-lg-6 mx-auto rounded mt-3' style={{ padding: '15px 10%' }}>
         <h3 className='text-white text-center text-danger mb-4'>BUY PGC</h3>
         <div>
