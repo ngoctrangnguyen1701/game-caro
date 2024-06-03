e605ea8b24e966653f1fb3bd778ac9459b00052c
Trang <ngoctrangnguyen1701@gmail.com>
2024-06-03 22:58:16 +0700

    change link serve from heroku to render

--- src/common/constants.js
+++ src/common/constants.js
@@ -2,7 +2,8 @@
 // export const linkServer = process.env.REACT_APP_LINK_SERVER_LOCAL
 // export const linkServer = process.env.REACT_APP_LINK_SERVER_HOSTING
 const LINK_SERVER_LOCAL = 'http://localhost:5000'
-const LINK_SERVER_HOSTING = 'https://server-play-game-caro.herokuapp.com'
+const LINK_SERVER_HOSTING = 'https://server-game-caro.onrender.com'
+// const LINK_SERVER_HOSTING = 'https://server-play-game-caro.herokuapp.com' //not use anymore
 
 export const linkServer = process.env.NODE_ENV === 'development' ? LINK_SERVER_LOCAL : LINK_SERVER_HOSTING
 // export const linkServer = LINK_SERVER_HOSTING
